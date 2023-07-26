8fd73381b63eb41bc711cc81e7039dd63e504ce3
Sergio de Sá Gallego <sdesagallego@gmail.com>
2023-07-26 20:40:57 +0100

    añadido tratameinto a la lista para deivision

---
+++ 2022(2023)/3_day/funcs.py
@@ -0,0 +1,15 @@
+
+
+
+
+
+
+valores					= {}
+letras					= 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+counter					= 0
+for x in letras:
+	counter 			+= 1
+	valores.update({x:counter})
+print(valores)
+
+
